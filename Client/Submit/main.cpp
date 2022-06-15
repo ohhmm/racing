7d29c2bc6efc12a85e9e28330a4a115e534984ff
Serg Kryvonos <sergeikrivonos@gmail.com>
2022-06-14 17:23:02 -0700

    Make server/client stubs

---
+++ Client/Submit/main.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+auto max_length = 1000000;
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc != 3)
+		{
+			std::cerr << "Usage: race_client <host> <port> < data\n";
+			return 1;
+		}
+
+		boost::asio::io_context io_context;
+
+		tcp::socket s(io_context);
+		tcp::resolver resolver(io_context);
+		boost::asio::connect(s, resolver.resolve(argv[1], argv[2]));
+		{
+			std::cout << "Enter message: ";
+			char request[max_length];
+			std::cin.getline(request, max_length);
+			size_t request_length = std::strlen(request);
+			boost::asio::write(s, boost::asio::buffer(request, request_length));
+		}
+		{
+			char reply[max_length];
+			size_t reply_length = boost::asio::read(s,
+				boost::asio::buffer(reply, request_length));
+			//std::cout << "Reply is: ";
+			std::cout.write(reply, reply_length);
+			std::cout << "\n";
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
